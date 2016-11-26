12016aef6540519b527cff3bab6651ee9a92f4d0
sergmaks <maximov.sv90@mail.ru>
2016-11-26 15:18:50 +0400

    Lesson 4: Working with form and generic views

--- polls/urls.py
+++ polls/urls.py
@@ -1,15 +1,22 @@
+"""
+    Настройка URLов
+"""
+
 from django.conf.urls import url
 
 from . import views
 
-app_name = 'polls'
+app_name = 'polls'  # пространтсво именя для приложения polls
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # r'^$'                      - регулярное выражение для поиска в URL
+    # views.IndexView.as_view()  - функция представления
+    # name='index'               - имя URL
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,58 @@
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+# Представление для общего списка вопросов
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
 
+# Представление для страницы определенного вопроса
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+# Представление отображения результатов голосования
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+"""
+Представление для обработки голоса
+
+:param request: объект запроса
+:param question_id: id вопроса
+:return: объект HttpResponse
+"""
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # Получаем объект вопроса по первичному ключу или Htttp404
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    # Если choice не находится в POST
+    except (KeyError, Choice.DoesNotExist):
+        # показывает страницу опроса с ошибкой, если не был выбран вариант ответа
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # Мы используем функцию reverse() при создании HttpResponseRedirect.
+        # Она принимает название URL-шаблона и необходимые аргументы для создания URL-а.
+        # например, reverse() вернет: '/polls/3/results/'
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
